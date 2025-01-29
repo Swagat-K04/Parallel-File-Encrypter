441b2d501b60834c165ad973d21fa84894c6e815
Swagat-K04 <swagatk1004@gmail.com>
2025-01-29 17:01:35 +0530

    Add shared memory

--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -4,6 +4,7 @@
 #include "Task.hpp"
 #include <queue>
 #include <memory>
+#include <atomic>
 
 class ProcessManagement {
 
@@ -11,8 +12,23 @@ class ProcessManagement {
     ProcessManagement();
     bool submitToQueue(unique_ptr<Task> task);
     void executeTasks();
+
   private:
-    queue<unique_ptr<Task>> taskQueue;
+    struct SharedMemory {
+      std::atomic<int> size;
+      char tasks[1000][256]; // 1000 files with each max, 256 bytes
+      int front;
+      int rear;
+
+      void printSharedMemory() {
+        std::cout << size << std::endl;
+      }
+    };
+
+    SharedMemory* sharedMem;
+    int shmFd;
+    const char* SHM_NAME = "/my_queue";
+    
 };
 
 #endif
